4b5c9b29fddb607ee36e5d6a9087c993c35ab3c0
eugeniogzc <eugenio.garza26@gmail.com>
2025-03-31 17:48:14 -0600

    arreglar direccion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/practica-frontend/', // 👈 esto es importante
+  base: '/practica-frontend/',
   plugins: [react()],
 })
+
